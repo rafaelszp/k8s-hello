62a4a8e508724783b9e392f6a6defbbf4cdc4480
Rafael Pereira <rafaelszp.info@gmail.com>
2017-11-16 10:57:14 -0200

    Creating properties file to reflect version in clock-service

--- clock/src/main/java/szp/rafael/k8s/clock/ClockRest.java
+++ clock/src/main/java/szp/rafael/k8s/clock/ClockRest.java
@@ -6,10 +6,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
-
+import java.util.Properties;
 
 
 @Path("/clock")
@@ -20,9 +24,19 @@ public class ClockRest {
   @GET
   public Response get(@Context HttpServletRequest req) throws UnknownHostException {
     if(ENABLED==1){
+      String version = "x.y.z";
+	  try {
+		InputStream file = ClockRest.class.getResourceAsStream("/app.properties");
+		System.out.println(file);
+		Properties props = new Properties();
+		props.load(file);
+		version = props.get("project.version").toString();
+	  } catch (IOException e) {
+		e.printStackTrace();
+	  }
 	  String ip = Inet4Address.getLocalHost().getHostAddress();
 	  LocalDateTime now = LocalDateTime.now();
-	  String info = "Clock from "+ip+". Current date and time is "+now;
+	  String info = "Clock v"+version+" from "+ip+". Current date and time is "+now;
 	  return Response.ok(info).build();
 	}
 	return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("ENABLED",ENABLED).build();
